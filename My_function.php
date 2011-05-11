7d86560b3e9498230be2bfe66aec4b84d059428e
Reunjit <rm265@njit.edu>
2011-05-11 17:04:30 +0000

    more corrections

--- My_function.php
+++ My_function.php
@@ -1,5 +1,6 @@
 <html>
 	<body>
+		<center>
 <?php
 interface headers {
 
@@ -17,21 +18,25 @@ class create implements headers
 		echo'<h1>'.$text.'</h1>';
 		}
 		elseif($level == 2){
-		echo'<h2>'.$text.'</h1>';
+		echo'<h2>'.$text.'</h2>';
 		}
 		elseif($level == 3){
-		echo'<h3>'.$text.'</h1>';
+		echo'<h3>'.$text.'</h3>';
 		}
 		elseif($level == 4){
-		echo'<h4>'.$text.'</h1>';
+		echo'<h4>'.$text.'</h4>';
 		}
+	}
+	function create_paragraph($text){
+		echo'<p>'.$text.'</p>';
 	}
 }
 $a = new create();
 $test = 'this is a test';
 $a->create_Header(4,$a->writeName());
 $a->create_header(1,$test);
+$a->create_paragraph("hello Eli");
 ?>
-
+		</center>
 	</body>
 </html>
