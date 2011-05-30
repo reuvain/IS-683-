a6ef3b7ee1eb98c7629e9a1ac751405d987d3286
Reunjit <rm265@njit.edu>
2011-05-30 20:51:01 +0000

    for Eli

--- checkInput.php
+++ checkInput.php
@@ -4,12 +4,29 @@ function check($input)
 	{
          	echo filter_var ($input,FILTER_SANITIZE_STRING);
         }
-
-        check($_POST['Text']);
-
+	
+function noCheck($input)
+	{
+		echo $input;
+	}
 ?>
-
-        <form action="" method="post">
-                <input type="text" name="Text" />
-        </form>
-
+	
+<table border=3 >
+	<tr><td>This Text Field has a filter:</td></tr>
+	<tr><td>
+        	<form action="" method="post">
+			 <input type="text" name="Filter" />
+		</form>
+	 	<?php check($_POST['Filter']);?>
+	</td></tr>
+</table>
+<br>
+<table border=3>
+	<tr><td>This Text Field doesn't:</td></tr>
+	<tr><td>	
+		<form action="" method="post">
+			<input type="text" name="noFilter" />
+		</form>
+		<?php noCheck($_POST['noFilter']);?>
+	</td></tr>
+</table>
