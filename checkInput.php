1576123c73bbf33028f895e511a20186797a21f3
Reunjit <rm265@njit.edu>
2011-06-12 22:45:36 +0000

    tester files

--- checkInput.php
+++ checkInput.php
@@ -1,32 +1,36 @@
 <?php
 
 function check($input)
-	{
-         	echo filter_var ($input,FILTER_SANITIZE_STRING);
-        }
-	
-function noCheck($input)
-	{
-		echo $input;
+{
+	if (isset($input)){
+	echo filter_var ($input,FILTER_SANITIZE_STRING);
 	}
+}
+
+function noCheck($input)
+{
+	echo $input;
+}
 ?>
-	
-<table border=3 >
-	<tr><td>This Text Field has a filter:</td></tr>
-	<tr><td>
-        	<form action="" method="post">
-			 <input type="text" name="Filter" />
-		</form>
-	 	<?php check($_POST['Filter']);?>
-	</td></tr>
+
+<table border=3>
+	<tr>
+		<td>This Text Field has a filter:</td>
+	</tr>
+	<tr>
+		<td>
+		<form action="" method="post"><input type="text" name="Filter" /></form>
+		<?php check($_POST['Filter']);?></td>
+	</tr>
 </table>
 <br>
 <table border=3>
-	<tr><td>This Text Field doesn't:</td></tr>
-	<tr><td>	
-		<form action="" method="post">
-			<input type="text" name="noFilter" />
-		</form>
-		<?php noCheck($_POST['noFilter']);?>
-	</td></tr>
+	<tr>
+		<td>This Text Field doesn't:</td>
+	</tr>
+	<tr>
+		<td>
+		<form action="" method="post"><input type="text" name="noFilter" /></form>
+		<?php noCheck($_POST['noFilter']);?></td>
+	</tr>
 </table>
